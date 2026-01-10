c9c607b07600fc6cf971626220036192f6f740b4
Rizdar <muhrizdar@gmail.com>
2026-01-10 21:58:19 +0700

    add listening and add (download) capabilities

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 import asyncio
 
-from client.getter import download_episode, get_episodes
-from podcli import PodcliApp
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from rich import print_json
+from rich.console import Console
+
+from podcli import (
+    download_episode,
+    get_all_episodes,
+    get_channel_data,
+    get_latest_episode,
+    listen_from_path,
+)
 
 
 async def main():
-    # app = PodcliApp()
-    # app.run()
-
-    feed_url = "https://feeds.captivate.fm/zeroknowledge/"
-    episodes = get_episodes(feed_url)
-    print(f"Found {len(episodes)} episodes.")
-    print(f"Downloading latest episode.")
-    print(f"Episode title: {episodes[0]['title']}")
-    await download_episode(episodes[0]["url"], episodes[0]["title"])
+    console = Console()
+    while True:
+        choice = await inquirer.select(
+            message="Do you want listen or add more to your library?",
+            choices=["Listen", "Add", Choice(value=None, name="Exit")],
+            default="Listen",
+        ).execute_async()
+        if choice:
+            if choice == "Add":
+                feed_url = await inquirer.text(message="Enter the URL:").execute_async()
+                # feed_url = "https://feeds.captivate.fm/zeroknowledge/"
+                channel_data = get_channel_data(feed_url)
+                latest_episode = get_latest_episode(feed_url)
+                if latest_episode:
+                    await download_episode(
+                        latest_episode["url"],
+                        latest_episode["title"],
+                        channel_data["title"],
+                    )
+                    console.print("Download success!", style="bold green")
+                else:
+                    console.print(
+                        "Error! Cannot download latest episode.", style="bold red"
+                    )
+            elif choice == "Listen":
+                console.print("WIP")
+                listen_from_path(
+                    "downloads/Zero Knowledge/Year in Review ZK Podcast in 2025  Beyond.mp3"
+                )
+            else:
+                console.print("Goodbye!", style="bold green")
+                break
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    # main()
--- podcli.py
+++ podcli.py
@@ -1,5 +1,135 @@
-import audio.playback as pb
+from pathlib import Path
+from time import sleep
+
+import feedparser
+import httpx
+import just_playback
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+    TransferSpeedColumn,
+)
+
+pb = just_playback.Playback()
+DOWNLOAD_DIR = Path("downloads")
+DOWNLOAD_DIR.mkdir(exist_ok=True)
 
 
-class PodcliApp:
-    pass
+def listen_from_path(path):
+    with Progress(
+        TextColumn("{task.fields[icon]}"),
+        BarColumn(),
+        TimeElapsedColumn(),
+    ) as progress:
+        if not pb.active:
+            pb.load_file(path)
+            pb.play()
+        else:
+            pb.resume()
+        duration = pb.duration
+        task_id = progress.add_task("Playing", total=duration, icon="▶")
+        while pb.active:
+            if pb.playing:
+                current_icon = "▶"
+                current_pos = pb.curr_pos
+                progress.update(task_id, completed=current_pos, icon=current_icon)
+            sleep(0.1)
+
+            if pb.paused:
+                current_icon = "Ⅱ"
+                pass
+
+
+def get_podcast_rss_from_itunes(query: str) -> str | None:
+    url = "https://itunes.apple.com/search"
+    params = {"term": query, "media": "podcast", "limit": 5}
+    response = httpx.get(url, params=params)
+    try:
+        json_response = response.json()
+        if json_response["resultCount"] > 0:
+            return json_response["results"][0]["feedUrl"]
+    except Exception:
+        return response.text
+    return None
+
+
+def get_channel_data(rss_url: str):
+    d = feedparser.parse(rss_url)
+
+    return {
+        "title": d.feed.title,
+        "link": d.feed.link,
+        "description": d.feed.description,
+    }
+
+
+def get_latest_episode(rss_url: str):
+    d = feedparser.parse(rss_url)
+    latest_episode = d.entries[0]
+
+    if latest_episode.enclosures[0].type == "audio/mpeg":
+        episode = {
+            "title": latest_episode.title,
+            "url": latest_episode.enclosures[0].href,
+            "date": latest_episode.published,
+            "duration": latest_episode.enclosures[0].length,
+            "link": latest_episode.link,
+            "description": latest_episode.description,
+            "id": latest_episode.id,
+        }
+        return episode
+    return None
+
+
+def get_all_episodes(rss_url):
+    d = feedparser.parse(rss_url)
+    episodes = []
+
+    for entry in d.entries:
+        if hasattr(entry, "enclosures") and len(entry.enclosures) > 0:
+            mp3_url = entry.enclosures[0].href
+            episodes.append(
+                {
+                    "title": entry.title,
+                    "url": mp3_url,
+                    "date": entry.published,
+                    "duration": entry.enclosures[0].length,
+                    "link": entry.link,
+                    "description": entry.description,
+                    "id": entry.id,
+                }
+            )
+    return episodes
+
+
+async def download_episode(url: str, title: str, channel_name: str):
+    filename = (
+        "".join(c for c in title if c.isalnum() or c in (" ", "_")).rstrip() + ".mp3"
+    )
+    save_path = DOWNLOAD_DIR / channel_name / filename
+    save_path.parent.mkdir(exist_ok=True)
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        DownloadColumn(),
+        TransferSpeedColumn(),
+        "eta",
+        TimeRemainingColumn(),
+    ) as progress:
+        async with httpx.AsyncClient(follow_redirects=True) as client:
+            async with client.stream("GET", url) as response:
+                total_size = int(response.headers.get("Content-Length", 0))
+                task_id = progress.add_task(
+                    f"Downloading {title[:20]}...", total=total_size
+                )
+                response.raise_for_status()
+                with open(save_path, "wb") as f:
+                    async for chunk in response.aiter_bytes():
+                        f.write(chunk)
+                        progress.update(task_id, advance=len(chunk))
